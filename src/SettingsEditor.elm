b218f9468f9d34334f448e316725983dc6703ae1
dexterlb <dexterlb@qtrp.org>
2020-12-10 16:09:33 +0200

    sync settings editor

--- src/SettingsEditor.elm
+++ src/SettingsEditor.elm
@@ -1,7 +1,9 @@
 module SettingsEditor exposing (Model, Msg, init, update, view, getSettings)
 
-import Settings as Settings exposing (Settings)
+import Settings as Settings exposing (Settings, SyncSettings)
 import Html as HI exposing (Html)
+import Html.Attributes as HA
+import Html.Events as HE
 
 import Utils exposing (..)
 import CommonComponents exposing (viewTZPicker)
@@ -26,11 +28,49 @@ update msg model = case msg of
 view : Model -> Html Msg
 view model = let s = model.settings in HI.div []
     [ checkbox s.hideDone
-        [ onClick (Evil (\m -> { m | settings = { s | hideDone = not s.hideDone } })) ]
+        [ onClick (Evil (\m -> { m | settings = { s | hideDone = not s.hideDone } }))
+        , HA.class "setting" ]
         [ HI.text "hide done items" ]
-    , HI.div []
+    , HI.div [ HA.class "setting" ]
         [ HI.label [] [ HI.text "default timezone" ]
         , viewTZPicker s.defaultTimezone (\v -> Evil (\m -> { m | settings = { s | defaultTimezone = v } }))
         ]
+    , viewSyncSettingsEditor s.sync (\ss m -> { m | settings = { s | sync = ss }})
     ]
 
+viewSyncSettingsEditor : SyncSettings -> (SyncSettings -> Model -> Model) -> Html Msg
+viewSyncSettingsEditor ss f = HI.div [ HA.class "ss-box" ]
+    [ HI.label [] [ HI.text "sync ss" ]
+
+    , HI.div [ HA.class "setting" ]
+        [ HI.label [] [ HI.text "server" ]
+        , HI.input
+            [ HA.type_ "text"
+            , HA.value ss.server
+            , HE.onInput (\v -> Evil (f { ss | server = v }))
+            ] []
+        ]
+
+    , HI.div [ HA.class "setting" ]
+        [ HI.label [] [ HI.text "space" ]
+        , HI.input
+            [ HA.type_ "text"
+            , HA.value ss.space
+            , HE.onInput (\v -> Evil (f { ss | space = v }))
+            ] []
+        ]
+
+    , HI.div [ HA.class "setting" ]
+        [ HI.label [] [ HI.text "secret" ]
+        , HI.input
+            [ HA.type_ "text"
+            , HA.value ss.secret
+            , HE.onInput (\v -> Evil (f { ss | secret = v }))
+            ] []
+        ]
+
+    , checkbox ss.enabled
+        [ onClick (Evil (f { ss | enabled = not ss.enabled }))
+        , HA.class "setting" ]
+        [ HI.text "enabled" ]
+    ]
